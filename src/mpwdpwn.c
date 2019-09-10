e67f17153e492ceec0c98a7e409d53c7b3bf7fc5
mr_chainman <rvreman7@me.com>
2019-09-10 22:42:32 +0400

    Add simple volumes scan check

--- src/mpwdpwn.c
+++ src/mpwdpwn.c
@@ -9,11 +9,19 @@ Copyright (C) techspider 2019. All rights reserved.
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <dirent.h>
 
 int main(int argc, char* argv[]) {
     fprintf(stdout, "%s", "mpwdpwn version 1.0. Copyright (C) mr_chainman (techspider) 2019.\n\n");
     if(geteuid() != 0) {
         fprintf(stderr, "%s", "Error: Missing permissions, rerun as root.\n");
+        return 1;
+    }
+    fprintf(stdout, "%s", "Scanning volumes...\n");
+    struct dirent *dEntry;
+    DIR *vDir = opendir("/Volumes");
+    if(vDir == NULL) {
+        fprintf(stderr, "%s", "Error: /Volumes directory does not exist, is this a Mac or is the file system corrupt?\n");
         return 1;
     }
 }
