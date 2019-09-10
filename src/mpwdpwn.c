c21732371f12c9fc4010d1aace1887e2fefcf5b2
mr_chainman <rvreman7@me.com>
2019-09-11 01:10:39 +0400

    Add user listing

--- src/mpwdpwn.c
+++ src/mpwdpwn.c
@@ -51,11 +51,39 @@ int main(int argc, char* argv[]) {
         strcpy(fullPath, "/Volumes/");
         strncat(fullPath, target->volumeName, 9 + strlen(target->volumeName));
         fprintf(stdout, "Selecting volume %s...\n", fullPath);
+        strcpy(target->volumePath, fullPath);
         if(dir_exists(fullPath) != 0) {
             fprintf(stderr, "%s", "Error: Volume does not exist!\n");
             continue;
         }
         validVolumeDetected = 0;
     }
+    char userDirPath[256];
+    char dbPathInFS[256];
+    strcpy(dbPathInFS, "/var/db/dslocal/nodes/Default/users");
+    strcpy(userDirPath, target->volumePath);
+    strncat(userDirPath, dbPathInFS, strlen(dbPathInFS) + strlen(userDirPath));
+    fprintf(stdout, "Checking %s for user plists...\n", userDirPath);
+    fprintf(stdout, "Enter the name of the user account you would like to unlock:\nAvailable Users:\n");
+    struct dirent * pluEntry;
+    DIR * pluDir = opendir(userDirPath);
+    if(pluDir == NULL) {
+        fprintf(stderr, "%s", "Error: User database is corrupt or this is not a valid volume.\n");
+        return 1;
+    }
+    while((pluEntry = readdir(pluDir)) != NULL) {
+        if(strncmp(pluEntry->d_name, "..", strlen(pluEntry->d_name)) == 0) continue;
+        else if(strncmp(pluEntry->d_name, ".", strlen(pluEntry->d_name)) == 0) continue;
+        else if(pluEntry->d_name[0] == '_') continue; //These are hidden users, do not list them.
+        else {
+            char username[128];
+            strcpy(username, pluEntry->d_name);
+            strtok(username, ".");
+            fprintf(stdout, "  %s\n", username);
+        }
+    }
+    fprintf(stdout, "\n");
+    closedir(pluDir);
+    
     return 0;
 }
